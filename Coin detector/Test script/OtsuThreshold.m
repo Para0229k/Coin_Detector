18e63ba95ab2411dc93daafbdcfbdbff210b47b8
Para0229 <para0229k@gmail.com>
2025-07-13 13:34:54 +0800

    Organize the file structure
    Delete old file.

--- Coin detector/Test script/OtsuThreshold.m
+++
@@ -1,9 +0,0 @@
-function [msk,thrsh] = OtsuThreshold(img)
-	% define the threshold by using histogram
-	hist = imhist(img);
-	thrsh = otsuthresh(hist);
-	thrsh = thrsh * 255;
-	
-	% apply threshold to img to make msk
-	msk = img > thrsh;
-end
